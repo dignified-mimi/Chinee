9c3d00c1008520a5fefeb54a460a494e0c44c01b
dignified-mimi <akujiezechinemerem@gmail.com>
2025-07-25 15:02:32 +0100

    Add files via upload

---
+++ cos assignment.py
@@ -0,0 +1,23 @@
+#  Grading system based on score
+
+#  Get user input 
+score  =  Int(input("Enter your score(0-100)  "))
+
+#  Determine the grade
+If  70  <=  score  <=  100:
+	   grade  =  "A"  
+elif  60  <=  score  <=  69:
+	      grade  "B"
+elif  50  <=  score  <=  59:
+	      grade  =  ''C"     
+elif  45  <=  score  <=  49:
+	      grade  =  "D"  
+elif  40  <=  score  <=  45:
+	      grade  =  "E"  
+elif  0  <=  score  <=  40:
+	    grade  =  "F"  
+else:
+	    grade  =  "invalid score. please enter a number betweeen 0 and 100''
+	    
+#  print result
+print  (" your grade is : '', grade)		               	 
